abef1a8e46254e18d4ddba531737a4d97acd53b3
AB-hex <74324857+AB-hex@users.noreply.github.com>
2021-01-05 20:29:21 +0200

    Add files via upload

---
+++ libgrades.c
@@ -0,0 +1,43 @@
+/*
+ ============================================================================
+ Name        : HW7.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked-list.h"
+//#include "liblinked-list.so"
+#include "grades.h"
+
+
+
+struct grades{
+	char* name;
+	int id;
+	int grade;	
+	int course;
+};
+
+struct grades* grades_init(struct grades *grades){
+
+
+};
+
+
+
+
+
+
+
+
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
